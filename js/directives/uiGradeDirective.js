74478111918c431326bca946af4979f261eb3957
TallesFaria <tallesbeginsc@gmail.com>
2017-07-19 00:46:45 -0300

    GradeService ok

---
+++ js/directives/uiGradeDirective.js
@@ -0,0 +1,23 @@
+angular.module("collegeAnalysis").directive("uiGrade", function () {
+	return {
+		require: "ngModel",
+		link: function (scope, element, attrs, ctrl) {
+			console.log(element);
+			var _formatGrade = function (grade) {
+				grade = grade.replace(/[^0-9]+/g, "");
+				if(grade.length > 2 && grade !== '100') {
+					grade = grade.substring(0,2) + "." + grade.substring(2);
+				}
+				if(grade.length > 5) {
+					grade = grade.substring(0,5)
+				}
+				return grade;
+			};
+
+			element.bind("keyup", function () {
+				ctrl.$setViewValue(_formatGrade(ctrl.$viewValue));
+				ctrl.$render();
+			});
+		}
+	};
+});
